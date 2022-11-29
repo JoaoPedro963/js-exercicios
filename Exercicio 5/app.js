194a93852a24bcde2e70de62f525faf12c9ede7f
João Pedro <112526011+JoaoPedro963@users.noreply.github.com>
2022-11-29 19:49:05 -0300

    Adição de legendas
    Adicionai mais informações para facilitar no processo de aprendizagem.

--- Exercicio 5/app.js
+++ Exercicio 5/app.js
@@ -1,5 +1,7 @@
 function NumeroChar(){
-    let text = document.getElementById("texto").value
-    let resp = text.length
+    let texto = document.getElementById("texto").value
+    let resp = texto.length
     document.getElementById("resposta").innerHTML = resp
 }
+// o .length  é acompanhado de uma variável com formato de texto,
+// que tem como função contar o número de caracteres da palavra
